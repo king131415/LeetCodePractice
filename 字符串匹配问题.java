d4d30e3a3a482317b300755afa7c3fe04239b1ce
king <1292203381@qq.com>
2021-04-27 21:43:06 +0800

    日常练习

---
+++ 字符串匹配问题.java
@@ -0,0 +1,11 @@
+package javaSePrictice;
+
+/**
+ * 有一个字符串 Str1="你好  好你 你哦好 你好"，和一个字符串Str2="你好"，问Str1是否包含
+ * Str2,若包含请返回1，否则就返回-1！
+ */
+public class 字符串匹配问题 {
+    public static void main(String[] args) {
+
+    }
+}
