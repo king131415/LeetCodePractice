f3815fd4b054b96707db1a49691c54cecf59a31c
king <1292203381@qq.com>
2021-05-10 22:10:35 +0800

    汽水瓶问题

---
+++ Test0510.java
@@ -0,0 +1,27 @@
+package javaDailyProctice.Test0510;
+import java.util.*;
+public class Test0510 {
+    //汽水瓶问题
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            while (sc.hasNext()) {
+                int num = sc.nextInt();
+                if(num == 0 || num < 1 || num > 100)
+                    break;
+                int m = num / 3;
+                int n = num % 3;
+                int count = m;
+                while(m > 0){
+                    int temp1 = m;
+                    int temp2 = n;
+                    m = (temp1 + temp2) / 3;
+                    n = (temp1 + temp2) % 3;
+                    count += m;
+                }
+                if(m == 0 && n == 2)
+                    count++;
+                System.out.println(count);
+            }
+            sc.close();
+        }
+}
