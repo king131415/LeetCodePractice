dc3488d5cdec46d767be8d4111ba6fa2071e5734
king <1292203381@qq.com>
2022-01-13 18:14:40 +0800

    日常练习

---
+++ Test0113_2.java
@@ -0,0 +1,27 @@
+
+
+import java.util.Arrays;
+
+public class Test0113_2 {
+    public static void main(String[] args) {
+        int[] arr={2,55,3,1,8,9,4,6};
+        insertSort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    private static void insertSort(int[] arr) {
+        for(int i=1;i<arr.length;i++){
+            int j=i-1;
+            int v=arr[i];
+            for(;j>=0;j--){
+                if(arr[j]>v){
+                    arr[j+1]=arr[j];
+                }else {
+                    break;
+                }
+            }
+            arr[j+1]=v;
+        }
+    }
+}
