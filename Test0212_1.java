7a074e02ce6b2a9d9af1a41f64ad54c470d641c0
king <1292203381@qq.com>
2022-02-12 21:34:28 +0800

    日常练习

---
+++ Test0212_1.java
@@ -0,0 +1,32 @@
+/**
+ * 给你两个二进制字符串，返回它们的和（用二进制表示）。
+ * 输入为 非空 字符串且只包含数字 1 和 0。
+ * 示例 1:
+ * 输入: a = "11", b = "1"
+ * 输出: "100"
+ * 示例 2:
+ * 输入: a = "1010", b = "1011"
+ * 输出: "10101"
+ */
+public class Test0212_1 {
+    public static void main(String[] args) {
+         String a="100";
+         String b="11";
+         String  c=addBinary(a,b);
+        System.out.println(c);
+    }
+    public static String addBinary(String a, String b) {
+        StringBuilder ans = new StringBuilder();
+        int ca = 0;  //用来存放计算后的结果
+        for(int i = a.length() - 1, j = b.length() - 1;i >= 0 || j >= 0; i--, j--) {
+            int sum = ca;
+            sum += i >= 0 ? a.charAt(i) - '0' : 0;
+            sum += j >= 0 ? b.charAt(j) - '0' : 0;
+            ans.append(sum % 2);
+            ca = sum / 2;
+        }
+        ans.append(ca == 1 ? ca : ""); //若ca不等于0，没有除尽，说明还有位数没加进去
+        return ans.reverse().toString();//反转字符串
+
+    }
+}
